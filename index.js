defb648e9199a0e0321803cf324317cd77147248
chandra11148 <cshekhar11148@gmail.com>
2024-03-02 19:28:02 +0530

    configuring socket

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const { join } = require("path");
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+const app = express();
+
+const server = createServer(app);
+
+const io =new Server(server);
+
+app.get("/",(req,res)=>{
+    res.sendFile(join(__dirname,'index.html'));
+})
+
+io.on('connection',(socket)=>{
+    console.log("a user connected");
+    socket.on("disconnect",()=>{
+        console.log("user Disconnected");
+    })
+})
+server.listen(8082,()=>{
+    console.log("Listening ...");
+})
+
+/* ---- */
+// const express = require('express');
+// const { createServer } = require('node:http');
+// const { join } = require('node:path');
+// const { Server } = require('socket.io');
+
+// const app = express();
+// const server = createServer(app);
+// const io = new Server(server);
+
+// app.get('/', (req, res) => {
+//   res.sendFile(join(__dirname, 'index.html'));
+// });
+
+// io.on('connection', (socket) => {
+//   console.log('a user connected');
+// });
+
+// server.listen(3000, () => {
+//   console.log('server running at http://localhost:3000');
+// });
