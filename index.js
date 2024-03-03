73eea5568964ef911629650553dd99834f31a60d
chandra11148 <cshekhar11148@gmail.com>
2024-03-03 18:40:25 +0530

    Broadcasting Demo

--- index.js
+++ index.js
@@ -17,12 +17,41 @@ io.on('connection',(socket)=>{
     socket.on("disconnect",()=>{
         console.log("user Disconnected");
     })
-})
+    socket.on("chat message", (msg)=>{
+        console.log(msg);
+        io.emit('chat message', msg);
+    });
+});
+// io.on('connection', (socket) => {
+//     socket.on('chat message', (msg) => {
+//       io.emit('chat message', msg);
+//     });
+//   });
 server.listen(8082,()=>{
     console.log("Listening ...");
 })
 
-/* ---- */
+// /* ---- */
+// // const express = require('express');
+// // const { createServer } = require('node:http');
+// // const { join } = require('node:path');
+// // const { Server } = require('socket.io');
+
+// // const app = express();
+// // const server = createServer(app);
+// // const io = new Server(server);
+
+// // app.get('/', (req, res) => {
+// //   res.sendFile(join(__dirname, 'index.html'));
+// // });
+
+// // io.on('connection', (socket) => {
+// //   console.log('a user connected');
+// // });
+
+// // server.listen(3000, () => {
+// //   console.log('server running at http://localhost:3000');
+// // });
 // const express = require('express');
 // const { createServer } = require('node:http');
 // const { join } = require('node:path');
@@ -37,7 +66,9 @@ server.listen(8082,()=>{
 // });
 
 // io.on('connection', (socket) => {
-//   console.log('a user connected');
+//   socket.on('chat message', (msg) => {
+//     io.emit('chat message', msg);
+//   });
 // });
 
 // server.listen(3000, () => {
